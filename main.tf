0e2510fe476da4c7b9f1d1359a5a96861f17b52c
mitesh <miteshzk@gmail.com>
2024-03-31 09:51:01 +0530

    Changes made to main tf

--- main.tf
+++ main.tf
@@ -5,10 +5,17 @@ provider "azurerm" {
 # Module for VM1
 module "vm1" {
   source = "./modules/JenkinsVM"
+
+prefix   = var.prefix
+location = var.location
+
 }
 
 # Module for VM2
 module "vm2" {
   source = "./modules/SonarVM"
+
+prefix   = var.prefix
+location = var.location
 
 }
