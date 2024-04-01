4f1b5755177a96ad9963c3233727098ec8bfad77
mitesh <miteshzk@gmail.com>
2024-03-31 23:49:20 -0700

    new changes pushed

--- modules/JenkinsVM/variables.tf
+++ modules/JenkinsVM/variables.tf
@@ -2,6 +2,6 @@ variable "prefix" {
   description = "The prefix which should be used for all resources"
 }
 
-variable "location2" {
+variable "location" {
   description = "The Azure Region in which all resources should be created."
 }
